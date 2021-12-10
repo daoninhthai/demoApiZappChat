b6374681d01917d7087859f95fdb9679e8a73b61
Đào Thái <thaimeo1131@gmail.com>
2021-12-10 23:55:38 +0700

    post

--- zalo-management-web-ui/src/pages/ManageAll/ManagePost/create/CreatePost.js
+++ zalo-management-web-ui/src/pages/ManageAll/ManagePost/create/CreatePost.js
@@ -1,17 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../../../style/style.css'
 
 import * as Yup from "yup";
 
 import {Button, Form, FormCheck, FormControl, Row} from "react-bootstrap";
-import React, {useEffect, useState} from 'react';
-import {useHistory, useParams} from 'react-router-dom';
-import '../../../../style/style.css'
+
 import {Formik} from 'formik';
+import React from 'react';
 import axios from "axios";
 import differenceInDays from 'date-fns/differenceInDays/index.js';
 import {differenceInYears} from "date-fns";
+import {useHistory} from 'react-router-dom';
 
-const EditUser = ({setResponseUser, setChildPage}) => {
+const CreateUser = ({setResponseUser, setChildPage}) => {
 
     const token = localStorage.getItem('jwttoken')
 
@@ -20,52 +21,37 @@ const EditUser = ({setResponseUser, setChildPage}) => {
 
     };
     const rootAPI = process.env.REACT_APP_SERVER_URL;
-    let {id} = useParams();
     const history = useHistory();
-    const [user, setUser] = useState({
+    const initialValues = {
         id: null,
         content: null,
         media: null,
         updated: null,  
-    });
-    useEffect(() => {
-        axios
-            .get(rootAPI + `/users/${id}`, {headers})
-            .then(function (response) {
-                setUser(response.data);
-                setGender(response.data.gender);
-            });
-    }, [id])
-
-    const [gender, setGender] = useState("");
-    const initialValues = {
-
-        content: post.content,
-        media: post.media,
-        updated: post.updated,
+      
        
 
     }
     const onSubmit = (values, {setSubmitting}) => {
-        let editUser = {
-      
+        let create = {
             content: values.content,
             media: values.media,
             updated: values.updated
-     
+    
         }
+
         axios
-            .put(rootAPI + `/users/${id}`, editUser, {headers})
+            .post(rootAPI + `/users`, create, {headers})
             .then((response) => {
                 setSubmitting(false);
                 setResponseUser({
                     id: response.data.id,
-                 
+                
+    
                     content: response.data.content,
                     media: response.data.media,
                     updated: response.data.updated,
+               
                   
-                    
                 });
                 setChildPage(null);
                 history.push("/user");
@@ -74,35 +60,37 @@ const EditUser = ({setResponseUser, setChildPage}) => {
     const ValidateSchema = Yup.object().shape({
         content: Yup.string()
         .max(1000)
-        .required()
-        .typeError("Content is required"),
+        .required('Required')
+        .typeError('Content is required'),
         media: Yup.string()
-        .max(1000)
-        .required()
-        .typeError("Media is required"),
-     
-        updated: Yup.date()
+            .max(1000)
+            .required('Required')
+            .typeError('Media is required'),
+       
+        
+        joinedDate: Yup.date()
             .required()
-            .typeError("Updated is required")
+            .typeError('updated is required')
+           
           
+        ,
     });
-
-    function onKeyDown(keyEvent) {
-        if ((keyEvent.charCode || keyEvent.keyCode) === 13) {
-            keyEvent.preventDefault();
-        }
+    const formValid = values => {
+        return (values.content === null
+            || values.media === null
+            || values.updated === null
+           
+        )
     }
-
     return (
         <div className={"container ps-5 d-block"}>
             <Row>
-                <h1 className={"text-primary mb-5"}>Edit Post</h1>
+                <h1 className={"text-primary mb-5"}>Create New Post</h1>
             </Row>
             <Row className={"mt-5 justify-content-start"}>
                 <Formik
                     initialValues={initialValues}
                     onSubmit={onSubmit}
-                    enableReinitialize={"true"}
                     validationSchema={ValidateSchema}
                 >
                     {({
@@ -115,23 +103,16 @@ const EditUser = ({setResponseUser, setChildPage}) => {
                           isSubmitting,
                           /* and other goodies */
                       }) => (
-                        <Form onSubmit={handleSubmit}
-                              onKeyDown={onKeyDown}
-                              className={"col-7"}
-                        >
-                              <Row className={"mb-3"}>
+                        <Form onSubmit={handleSubmit} className={"col-7"}>
+                            <Row className={"mb-3"}>
                                 <p className={"w-25"}>Media</p>
                                 <FormControl
-                                   
                                     aria-label="Username"
                                     aria-describedby="basic-addon1"
                                     className={"w-75"}
-                                    name="media"
-                                    style={{backgroundColor: "#eff1f5"}}
-                                    value={values.media}
-                                    onChange={handleChange}
-                                    onError={errors}
+                                    name={"media"}
                                     onBlur={handleBlur}
+                                    onChange={handleChange}
                                     isValid={touched.media && !errors.media}
                                     isInvalid={touched.media && errors.media}
                                 />
@@ -144,19 +125,15 @@ const EditUser = ({setResponseUser, setChildPage}) => {
                                     </div>
                                 ) : null}
                             </Row>
-                                <Row className={"mb-3"}>
+                            <Row className={"mb-3"}>
                                 <p className={"w-25"}>Content</p>
                                 <FormControl
-                                   
                                     aria-label="Username"
                                     aria-describedby="basic-addon1"
                                     className={"w-75"}
-                                    name="content"
-                                    style={{backgroundColor: "#eff1f5"}}
-                                    value={values.content}
-                                    onChange={handleChange}
-                                    onError={errors}
+                                    name={"content"}
                                     onBlur={handleBlur}
+                                    onChange={handleChange}
                                     isValid={touched.content && !errors.content}
                                     isInvalid={touched.content && errors.content}
                                 />
@@ -169,19 +146,17 @@ const EditUser = ({setResponseUser, setChildPage}) => {
                                     </div>
                                 ) : null}
                             </Row>
-
                          
-                       
+                    
                             <Row className="mb-3">
                                 <p className={"w-25"} id="basic-addon1">
                                     Updated
                                 </p>
                                 <FormControl
                                     type={"date"}
-                                    aria-describedby="basic-addon1"
                                     className={"w-75"}
                                     name={"updated"}
-                                    value={values.updated}
+                                    placeholder={"1999-01-01"}
                                     onChange={handleChange}
                                     onBlur={handleBlur}
                                     isValid={touched.updated && !errors.updated}
@@ -196,24 +171,25 @@ const EditUser = ({setResponseUser, setChildPage}) => {
                                     </div>
                                 ) : null}
                             </Row>
-                         
+                          
                             <Row className={"justify-content-end"}>
                                 <Button
                                     variant={"primary"}
-                                    type="submit"
+                                    type={"submit"}
                                     style={{width: "100px"}}
+                                    disabled={formValid(values)}
                                 >
                                     SAVE
                                 </Button>
                                 <Button
                                     variant={"secondary"}
+                                    onClick={() => {
+                                        setChildPage(null);
+                                        history.push("/user");
+                                    }}
                                     type={"submit"}
                                     className={"ms-5"}
                                     style={{width: "100px"}}
-                                    onClick={() => {
-                                        setChildPage(null);
-                                        history.push("/user")
-                                    }}
                                 >
                                     CANCEL
                                 </Button>
@@ -227,4 +203,4 @@ const EditUser = ({setResponseUser, setChildPage}) => {
     );
 };
 
-export default EditUser;
+export default CreateUser;
